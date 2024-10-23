72107572b11ac237a3b317af05f2ac38deea2799
AlexKovalenkoBY <kovalenkoam@gmail.com>
2024-10-23 17:28:21 +0300

    правил и пробовал

---
+++ sec2/src/main/java/com/example/sec/AuthenticationController.java
@@ -0,0 +1,62 @@
+package com.example.sec;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+<<<<<<< HEAD
+import org.springframework.security.core.context.SecurityContextHolder;
+=======
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.ldap.userdetails.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+<<<<<<< HEAD
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+=======
+    private JwtTokenUtil jwtUtil;
+
+    @PostMapping("/authenticate2")
+    public ResponseEntity<?> authenticate(@RequestBody AuthRequest authenticationRequest) {
+>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+<<<<<<< HEAD
+        String token = jwtUtil.generateToken(authenticationRequest.getUsername());
+
+        return ResponseEntity.ok(new AuthenticationResponse(token));
+    }
+=======
+        String token = jwtUtil.generateTokenViaUsername(
+            authenticationRequest.getUsername()
+            );
+
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+    @GetMapping("/authenticate2")
+    public String authenticateGet(@RequestBody AuthRequest authenticationRequest, @AuthenticationPrincipal Person person) {
+		return "Hello, " + person.getSn() + "!!";
+        
+}
+>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2
+}
---
+++ sec2/src/main/java/com/example/sec/JwtUtil.java
@@ -0,0 +1,77 @@
+package com.example.sec;
+
+<<<<<<< HEAD
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+=======
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+<<<<<<< HEAD
+import java.util.function.Function;
+=======
+>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+<<<<<<< HEAD
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+<<<<<<<< HEAD:src/main/java/com/example/sec/JwtUtil.java
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+========
+    public String generateTokenViaUsername(String username) {
+       return "";
+>>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2:src/main/java/com/example/sec/JwtTokenUtil.java
+    }
+=======
+>>>>>>> 710b1ce1e007dd0bb2e1f8d08c70d0f2901110d2
+}
